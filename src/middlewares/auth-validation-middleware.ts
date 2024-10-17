9139fe601d9269c2dfdcbe3276cf303c26085748
seeyous00n <borovikov.learn@gmail.com>
2024-10-17 04:25:54 +0300

    fix: bag in authValidationMiddleware

--- src/middlewares/auth-validation-middleware.ts
+++ src/middlewares/auth-validation-middleware.ts
@@ -1,17 +1,19 @@
 import { NextFunction, Request, Response } from 'express';
 
 export const authValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const log = 'admin';
-  const pass = 'qwerty';
+  const LOG = 'admin';
+  const PASS = 'qwerty';
+  const BASIC = 'Basic';
 
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     res.status(401).json('Unauthorized');
   }
+
   if (authHeader) {
     const authorization = authHeader.split(' ');
     const decodedData = Buffer.from(authorization[1], 'base64').toString('utf-8').split(':');
-    if (decodedData[0] !== log || decodedData[1] !== pass) {
+    if (decodedData[0] !== LOG || decodedData[1] !== PASS || authorization[0] !== BASIC) {
       res.status(401).json('Unauthorized');
     }
   }
--- src/validations/data-validation.ts
+++ src/validations/data-validation.ts
@@ -37,5 +37,6 @@ export const postDataUpdateValidation = [
   titleValidator,
   shortDescriptionValidator,
   contentValidator,
+  isBlogId,
   errorsValidationMiddleware,
 ];
