a778767036210371819d1c2cd5e79519311e531c
seeyous00n <borovikov.learn@gmail.com>
2024-10-17 04:11:02 +0300

    fix: bag in authValidationMiddleware

--- src/middlewares/auth-validation-middleware.ts
+++ src/middlewares/auth-validation-middleware.ts
@@ -5,14 +5,15 @@ export const authValidationMiddleware = (req: Request, res: Response, next: Next
   const pass = 'qwerty';
 
   const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    res.status(401).json('Unauthorized');
+  }
   if (authHeader) {
     const authorization = authHeader.split(' ');
     const decodedData = Buffer.from(authorization[1], 'base64').toString('utf-8').split(':');
-
     if (decodedData[0] !== log || decodedData[1] !== pass) {
       res.status(401).json('Unauthorized');
     }
   }
-
   next();
 };
