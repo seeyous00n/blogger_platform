ec1f883dd4ade1d656f26ed6eb789ac03f4b0dba
seeyous00n <borovikov.learn@gmail.com>
2024-10-17 17:42:03 +0300

    add types for setndhrowrror

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const setAndThrowError = ({ message, status }: any) => {
+export const setAndThrowError = ({ message, status }: { message: string, status: number }) => {
   const err = { message, status };
   throw new Error(JSON.stringify(err));
 };
