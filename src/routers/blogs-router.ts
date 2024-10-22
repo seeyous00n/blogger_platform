058df02f64edcb0143de755ead0801c20a9651cd
seeyous00n <borovikov.learn@gmail.com>
2024-10-22 21:39:00 +0300

    feat: add new rout

--- src/routers/blogs-router.ts
+++ src/routers/blogs-router.ts
@@ -7,6 +7,7 @@ const blogsRouter = Router();
 
 blogsRouter.get('/', blogsController.getBlogs);
 blogsRouter.get('/:id', blogsController.getBlog);
+blogsRouter.get('/:id/posts', blogsController.getBlogs);
 blogsRouter.post('/', authValidationMiddleware, blogDataValidation, blogsController.createBlog);
 blogsRouter.put('/:id', authValidationMiddleware, blogDataValidation, blogsController.updateBlog);
 blogsRouter.delete('/:id', authValidationMiddleware, blogsController.deleteBlog);
