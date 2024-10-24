fb8d351525da90539e7a54ab21f2da98e98ca9fc
seeyous00n <borovikov.learn@gmail.com>
2024-10-25 02:38:18 +0300

    change type

--- src/models/ViewQueryModel.ts
+++ src/models/ViewQueryModel.ts
@@ -1,10 +1,10 @@
 import { BlogsViewDto } from '../dtos/blogs-view-dto';
-import { PostType } from '../types/post-types';
+import { PostsViewDto } from '../dtos/posts-view-dto';
 
 export type QueryViewModel = {
   pagesCount: number,
   page: number,
   pageSize: number,
   totalCount: number,
-  items: BlogsViewDto[] | PostType[]
+  items: BlogsViewDto[] | PostsViewDto[]
 }
