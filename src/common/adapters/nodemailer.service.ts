109b7de5f0e79c062e49294ef92826f3817ef153
seeyous00n <borovikov.learn@gmail.com>
2024-11-02 03:00:36 +0300

    test

--- src/common/adapters/nodemailer.service.ts
+++ src/common/adapters/nodemailer.service.ts
@@ -14,16 +14,19 @@ const smtpConfig: SMTPTransport.Options = {
 
 class NodemailerService {
   sendEmail = async (to: string, link: string) => {
-
-    const registrationHtmlTemplate = `<h1>Hi!</h1><div><a href="${link}">Confirm</a></div>`;
-    const transporter = nodemailer.createTransport(smtpConfig);
-    transporter.sendMail({
-      from: SETTINGS.SMTP_EMAIL,
-      to: to,
-      subject: 'Activation link',
-      text: '',
-      html: registrationHtmlTemplate,
-    });
+    try {
+      const registrationHtmlTemplate = `<h1>Hi!</h1><div><a href="${link}">Confirm</a></div>`;
+      const transporter = nodemailer.createTransport(smtpConfig);
+      await transporter.sendMail({
+        from: SETTINGS.SMTP_EMAIL,
+        to: to,
+        subject: 'Activation link',
+        text: '',
+        html: registrationHtmlTemplate,
+      });
+    } catch (e) {
+      throw e;
+    }
   };
 }
 
