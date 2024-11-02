0a9c2741888b1d9e214102e6394fd9ea37bb7dbd
seeyous00n <borovikov.learn@gmail.com>
2024-11-02 12:49:20 +0300

    feat: add validation quryStringPagination

--- src/blogs/blogs.router.ts
+++ src/blogs/blogs.router.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import { blogsController } from './blogs.controller';
 import { blogDataValidation, postDataValidationWithoutId } from '../common/validation/data.validation';
 import { authBaseGuard } from '../common/middlewares/guards/authBase.guard';
+import { queryStringPaginationValidation } from '../common/validation/queryStringPagination.validation';
 
 const blogsRouter = Router();
 
-blogsRouter.get('/', blogsController.getBlogs);
+blogsRouter.get('/', queryStringPaginationValidation, blogsController.getBlogs);
 blogsRouter.get('/:id', blogsController.getBlog);
-blogsRouter.get('/:id/posts', blogsController.getPostsByBLog);
+blogsRouter.get('/:id/posts', queryStringPaginationValidation, blogsController.getPostsByBLog);
 blogsRouter.post('/', authBaseGuard, blogDataValidation, blogsController.createBlog);
 blogsRouter.post('/:id/posts', authBaseGuard, postDataValidationWithoutId, blogsController.createPostByBlog);
 blogsRouter.put('/:id', authBaseGuard, blogDataValidation, blogsController.updateBlog);
