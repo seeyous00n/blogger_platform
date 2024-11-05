2daa9416d93b6453ff888d56b37c565abb96f89a
seeyous00n <borovikov.learn@gmail.com>
2024-11-05 18:50:53 +0300

    test: /registration + /login

--- __tests__/e2e/auth.api.test.ts
+++ __tests__/e2e/auth.api.test.ts
@@ -1,9 +1,9 @@
-import {req} from '../utils/test-helpers';
-import {describe} from 'node:test';
-import {HTTP_STATUS_CODE, ROUTER_PATHS} from '../../src/common/settings';
-import {client, runDB} from '../../src/db';
-import {UserCreateModel} from '../../src/users/models/userCreate.model';
-import {nodemailerService} from '../../src/common/adapters/nodemailer.service';
+import { req } from '../utils/test-helpers';
+import { describe } from 'node:test';
+import { HTTP_STATUS_CODE, ROUTER_PATHS } from '../../src/common/settings';
+import { client, runDB } from '../../src/db';
+import { UserCreateModel } from '../../src/users/models/userCreate.model';
+import { nodemailerService } from '../../src/common/adapters/nodemailer.service';
 
 describe('/auth', async () => {
   beforeAll(async () => {
@@ -18,8 +18,8 @@ describe('/auth', async () => {
     await client.close();
   });
 
-  it('/registration: should create user', async () => {
-    const data: UserCreateModel = {login: 'login11', password: 'login11111', email: 'borovikov.live@gmail.com'};
+  it('/registration, /login', async () => {
+    const data: UserCreateModel = { login: 'login11', password: 'login11111', email: 'borovikov.live@gmail.com' };
     nodemailerService.sendEmail = jest.fn().mockImplementationOnce((): Promise<void> => Promise.resolve());
 
     await req
@@ -34,10 +34,22 @@ describe('/auth', async () => {
     expect(user.body.items).toEqual([
       {
         id: expect.any(String),
-        login: 'login11',
-        email: 'borovikov.live@gmail.com',
+        login: data.login,
+        email: data.email,
         createdAt: expect.any(String),
       },
     ]);
+
+    const loginUser = await req
+      .post(`${ROUTER_PATHS.AUTH}/login`)
+      .send({
+        "loginOrEmail": data.login,
+        "password": data.password
+      })
+      .expect(HTTP_STATUS_CODE.OK_200)
+
+    expect(loginUser.body).toEqual({
+      "accessToken": loginUser.body.accessToken,
+    })
   });
 });
