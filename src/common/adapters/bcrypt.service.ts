0fbc4f4ce8223b33cebf60475b4c92ee2afc0701
seeyous00n <borovikov.learn@gmail.com>
2024-11-25 03:53:34 +0300

    feat: add method compare password

--- src/common/adapters/bcrypt.service.ts
+++ src/common/adapters/bcrypt.service.ts
@@ -4,3 +4,6 @@ export const generatePasswordHash = async (password: string): Promise<string> =>
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
+
+export const compareHash = async (password: string, hash: string) =>
+  await bcrypt.compare(password, hash);
